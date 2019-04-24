6964c0d9caf6e496916410293fd6848685781d6b
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-04-24 11:32:00 +1000

    made timer a method

--- keepTime.rb
+++ keepTime.rb
@@ -1,8 +1,10 @@
-time = Time.new(0)
-start_at = 0 # change this value
+def timer
+  time = Time.new(0)
+  start_at = 0 # change this value
 
-start_at.upto(1000) do |seconds|
-  print (time + seconds).strftime('%M:%S')
-  sleep(0.1)
-  print "\r"
+  start_at.upto(1000) { |seconds|
+    score = print (time + seconds).strftime('%M:%S')
+    sleep(0.1)
+    print "\r"
+  }
 end

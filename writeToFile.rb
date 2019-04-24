45719acf52eee0327fcecef8136025850b12f10f
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-04-24 15:49:38 +1000

    blarg

--- writeToFile.rb
+++ writeToFile.rb
@@ -7,14 +7,34 @@ require "./keepTime.rb"
 # file.close #closes file when done
 puts "Enter you name: "
 name = gets.chomp.capitalize
-
 score = 120
+current = [name, score]
+
 name_score = Array.new
+name_score = [["jo", 120,], ["matt", 70], ["Roh", 130], ["Dave", 50]]
+name_score.push(current)
 x, y = 0, 0
 name_score.sort! {|score1,score2| score2[1] <=> score1[1]}
-high_score = File.open("HighScores.txt", "a+")
-p high_score
 
+#file = File.open("HighScores.txt", "a+") {|scores| scores.puts current_player}
+
+#File.open("HighScores.txt", "a+") {|file| file.puts current} #write score into file
+#p name_score
+
+File.open("HighScores.txt", "a+") { |file| file.puts "#{name_score}"} #push name_score into file
+#line = File.foreach("HighScores.txt")#.first(5) #prints first 5 lines
+#name_score.sort! {|score1,score2| score2[1] <=> score1[1]}
+#puts line.first(5)
+count = 0
+fileObj = File.open("HighScores.txt", "r")
+while (count < 5 && line = fileObj.gets.chomp)
+  print (line)
+  print "................"
+  print (line)
+  puts " "
+  count += 1
+end
+fileObj.close
 
 # arr.push(current_player)
 # arr.each {|player, score| puts player[score]}

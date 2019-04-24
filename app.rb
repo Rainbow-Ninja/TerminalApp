2107a3b4b69bbed181821e63d2480921adbe6d22
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-04-25 01:08:07 +1000

    added welcome page, instructions, puppy

--- app.rb
+++ app.rb
@@ -9,7 +9,10 @@ end
 require 'io/console'
 require './mazes.rb'
 require './game.rb'
+require './congratsMsg.rb'
+require './welcomePage.rb'
 
+welcome
 game = MazeGame.new($mazes["maze1"])
 game.print_board()
 game.input()

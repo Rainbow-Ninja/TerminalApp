27717188188d8656f6f36d2cd6d97edd824b401c
Rainbow-Ninja <scaffelpike@yahoo.com.au>
2019-04-24 14:45:07 +1000

    comparing scores in 2d array

--- writeToFile.rb
+++ writeToFile.rb
@@ -7,19 +7,21 @@ require "./keepTime.rb"
 # file.close #closes file when done
 puts "Enter you name: "
 name = gets.chomp.capitalize
+
 score = 120
-name_score = []
+name_score = Array.new
 x, y = 0, 0
-name_score[x][y]
-name_score[name][score]
-puts name_score
-# current_player = {"name" => name, "score" => score}
+name_score.sort! {|score1,score2| score2[1] <=> score1[1]}
+high_score = File.open("HighScores.txt", "a+")
+p high_score
+
+
 # arr.push(current_player)
 # arr.each {|player, score| puts player[score]}
-#puts current_player[score]
+# puts current_player[score]
 # file = File.open("HighScores.txt", "a+") {|scores| scores.puts current_player} #curly brackets opens and closes the file in one.
 
 # lines = File.foreach("HighScores.txt").first(3) #prints first 5 lines of file
 # puts lines
 
-#go through hash and print highest 5 scores
+# go through hash and print highest 5 scores

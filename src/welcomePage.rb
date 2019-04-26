c151ce7ecc550c945ec521ba23d5247647fe2b45
David James Baines <bainezy94@gmail.com>
2019-04-26 20:24:40 +1000

    Made welcome array across multiple lines instead of 1. Made Objective message more dry.

--- src/welcomePage.rb
+++ src/welcomePage.rb
@@ -1,15 +1,26 @@
 def welcome
     system("clear")
     welcome = "\nWelcome to\n\n".each_char {|c| print c.red ; sleep(0.4) }
-    welcome_arr = ["               __  __   ___                       .--.   _..._              ","              |  |/  `.'   `.                     |__| .'     '.   .--./)   ","              |   .-.  .-.   '                    .--..   .-.   . /.''\\    ","         __   |  |  |  |  |  |    __              |  ||  '   '  || |  | |   ","      .:--.'. |  |  |  |  |  | .:--.'.  .--------.|  ||  |   |  | \`-' /    ","     / |   \  ||  |  |  |  |  |/ |  \ | |____      ||  ||  |   |  | /(''`     ","     `-'__ | ||  |  |  |  |  |`- __ | |     /   / |  ||  |   |  | \ '---.   ","      .'.''| ||__|  |__|  |__| .'.''| |   .'   /  |__||  |   |  |  /'""'.\  ","     / /   | |_               / /   | |_ /    /___    |  |   |  | ||   || ","     \ \._,\ '/                     \ \._,\ '/|         |   |  |   |  | \'. __//  ","      `--'  `-                 `--'  `- |_________|   '--'   '--'  `'---'   "]
+    welcome_arr = [
+        "               __  __   ___                       .--.   _..._              ",
+        "              |  |/  `.'   `.                     |__| .'     '.   .--./)   ",
+        "              |   .-.  .-.   '                    .--..   .-.   . /.''\\    ",
+        "         __   |  |  |  |  |  |    __              |  ||  '   '  || |  | |   ",
+        "      .:--.'. |  |  |  |  |  | .:--.'.  .--------.|  ||  |   |  | \`-' /    ",
+        "     / |   \  ||  |  |  |  |  |/ |  \ | |____      ||  ||  |   |  | /(''`     ",
+        "     `-'__ | ||  |  |  |  |  |`- __ | |     /   / |  ||  |   |  | \ '---.   ",
+        "      .'.''| ||__|  |__|  |__| .'.''| |   .'   /  |__||  |   |  |  /'""'.\  ",
+        "     / /   | |_               / /   | |_ /    /___    |  |   |  | ||   || ",
+        "     \ \._,\ '/                     \ \._,\ '/|         |   |  |   |  | \'. __//  ",
+        "      `--'  `-                 `--'  `- |_________|   '--'   '--'  `'---'   "
+    ]
+
     welcome_arr.each {|line| puts line.cyan; sleep(0.5)}
 
     sleep(5)
     system("clear")
-    puts "          It's been a long day!\n\n"
-    print "  Use the"
-    print " arrow keys".red
-    puts " to drive home through\n\n"
-    puts "the maze to get home and cuddle your puppy #{$goal_icon}"
+    puts "It's been a long day!\n\n".center(45)
+    puts "Use the #{"arrow keys".red} to drive home through\n\n".center(55)
+    puts "the maze to get home and cuddle your puppy #{$goal_icon}".center(45)
     sleep(7)
 end
